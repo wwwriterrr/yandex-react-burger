9dc1ce29b28785ca9a1d5c591106fe2744b53888
wwwriterrr <royal.writer@yandex.ru>
2024-10-20 20:43:55 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import './App.css'
+import styles from './App.module.css';
+
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <div className={styles.wrap}>My app</div>
+    </>
+  )
+}
