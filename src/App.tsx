ea240811fe63f863f3d1619cca2d99075c79a8d5
wwwriterrr <royal.writer@yandex.ru>
2024-10-21 23:15:02 +0100

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 // import './App.css'
-import styles from './App.module.css';
+// import styles from './App.module.css';
+import { MainPage } from './pages';
 
 
 export const App: React.FC = () => {
   return (
     <>
-      <div className={styles.wrap}>My app</div>
+      <MainPage />
     </>
   )
 }
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,14 @@
+import { PageHeader } from '../../components';
+import styles from './main-page.module.css';
+
+
+export const MainPage = () => {
+    return (
+        <div className={styles.page}>
+            <PageHeader />
+            <div className={styles.wrap}>
+
+            </div>
+        </div>
+    )
+}
